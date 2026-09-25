106a1423bc45ca58ce99a3bdde729ce994b62d2e
danipernas-tech <danielpernasbenito@gmail.com>
2026-09-25 17:06:36 +0200

    Bucle for, importar datos en el csv

--- TASKS.md
+++ TASKS.md
@@ -1,6 +1,15 @@
 Fase 01
-- Instalar yfinance en VSCode "pip install yfinance"
-- Importar yfinance con alias yf
-- Descargar histórico de los ticker con fy.download()
+- He instalado yfinance en VSCode "pip install yfinance"
+- Para descargar el histórico de los ticker uso yf.download()
 
-* La ventana de los 4 ETFs Arranca el 16 de Julio de 2021
+* La ventana de los 4 ETFs Arranca el 16 de Julio de 2021 limitada por el histórico del ETF de oro
+
+1.1 He usado un diccionario para guarda la clave - valor de el Ticker de cada ETF y su peso en la cartera
+
+1.2 Creamos un .csv donde añadimos una columna para "Fecha" "Ticker" y "Precio"
+
+1.3 Con un bucle for recorremos la clave de cada ETF y nos descargamos los datos
+
+1.4 Guardamos en "cierres" los datos de "Close" que hay en las descargas que hemos hecho con el yf.download
+
+1.5 Recorremos "cierres" con otro for para cada clave y añadimos la fila en el .csv con el orden fecha, ticker, precio
--- src/prueba.py
+++ src/prueba.py
@@ -1,5 +1,6 @@
 import yfinance as yf
 import pandas as pd
+import csv
 
 '''
 #BUSQUEDA Código de Instrumento de Reuters .DE
@@ -27,7 +28,9 @@ datos_oro = yf.download("PPFB.DE", period="max")
 print(datos_oro)
 
 '''
-
+'''
+RECORRER EL DICCIONARIO PARA COMPROBAR E IMPRIMIR LOS DATOS DE CADA
+ETF CON PERIÓDO MÁXIMO
 
 etfs = {
     "EUNL.DE" : 0.70,
@@ -40,6 +43,31 @@ for ticker in etfs:
     datos = yf.download(ticker, period="max")
     print(datos)
 
+SIGUIENTE: 
+QUEDARNOS SOLO CON CLOSED
+RECORRER E IMPRIMIR CADA FILA "FECHA, TICKER, PRECIO"
+GUARDAR EN CSV
+
+'''
+
+etfs = {
+    "EUNL.DE" : 0.70,
+    "IS3N.DE" : 0.15,
+    "PPFB.DE" : 0.10,
+    "VVSM.DE" : 0.05
+    }
+
+
+with open("data/datos_etfs.csv", "w", newline="", encoding="utf-8") as archivo:
+    escritor = csv.writer(archivo)
+    escritor.writerow(["Fecha", "Ticker", "Precio"])
+    for ticker in etfs:
+            datos = yf.download(ticker, period="max", multi_level_index=False)
+            if datos is not None and "Close" in datos:
+                cierres = datos["Close"]
+                #print(cierres)
+                for fecha ,precio in cierres.items():
+                        escritor.writerow([fecha, ticker, precio])
 
 
 
